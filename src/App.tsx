1dd98b64b74c75ad257524534aa5b70629cd6d9f
Biruk <biruk.rcd.1912@gamil.com>
2023-12-23 13:27:15 +0300

    added init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./Components/Footer";
+import { Hero } from "./Components/Hero";
+import Main from "./Components/Main";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Main.tsx
@@ -0,0 +1,66 @@
+export default function Main() {
+  return (
+    <div className="grotesk max-w-8xl mx-auto">
+      <section className="w-full text-black">
+        <div className="mt-0 bg-white lg:mt-40">
+          <div className="text-black">
+            <div
+              className="
+                max-w-9xl
+                mx-auto
+                flex
+                flex-col
+                items-center
+                justify-center
+                px-5
+              "
+            >
+              <div className="mr-0 mb-6 w-full py-4 text-center lg:w-2/3">
+                <h2 className="mb-4 text-4xl font-bold sm:text-5xl">
+                  Sem enim cursus orci at.
+                </h2>
+                <p className="mb-4 text-lg leading-relaxed">
+                  In ullamcorper magna nunc, non molestie augue feugiat eget.
+                  Mauris, vitae et, vitae et cursus amet tincidunt feugiat
+                  nulla. Senectus maecenas diam risus sodales dictum eu. Eget
+                  cursus sit bibendum pulvinar faucibus vitae nam sed. Faucibus
+                  vel laoreet.
+                </p>
+                <a href="/" className="underline-blue font-semibold">
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-white text-black">
+          <div className="mt-32">
+            <div className="mx-auto flex flex-col px-5 py-24 text-left lg:flex-row">
+              <div className="hidden lg:inline-block lg:w-full lg:max-w-xl">
+                <img
+                  src="https://preview.colorlib.com/theme/breed2/img/about-us.png"
+                  alt="img"
+                />
+              </div>
+              <div className="flex flex-col pt-0 text-left lg:w-1/2 lg:flex-grow lg:items-start lg:pl-16 lg:pl-24 lg:pt-24">
+                <h2 className="mb-4 text-4xl font-bold leading-none sm:text-5xl">
+                  Software Developer
+                </h2>
+                <p className="mb-8 font-semibold leading-relaxed text-black">
+                  Also signs his face were digns fish don't first isn't over
+                  evening hath divided days light darkness gathering moved dry
+                  all darkness then fourth can't create d forth Also signs Also
+                  signs his face were moltenus Also signs his face.
+                </p>
+              </div>
+              <div className="inline-block lg:hidden lg:w-full lg:max-w-xl">
+                <img src="/images/placeholder.png" alt="img" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
